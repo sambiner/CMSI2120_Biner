ffd2a6b0dbe76b13b1e72972925cfd5ac96a6b1a
Sam Biner <samuel_biner@student.davincischools.org>
2022-09-07 14:04:57 -0700

    Updating Classwork

---
+++ Classwork/Classwork1/HelloSayer.java
@@ -0,0 +1,12 @@
+/*
+File Name: HelloSayer.java
+Description: A simple program that prints out "Hello World!"
+Date: 08/31/2022
+Developer: Sam Biner
+*/
+
+class HelloSayer {
+  public static void main(String args[]){
+    System.out.println("\n Hello World! \n");
+  }
+}
--- Classwork/Classwork1/HelloSayerV2.java
+++ Classwork/Classwork1/HelloSayerV2.java
@@ -1,3 +1,10 @@
+/*
+File Name: HelloSayerV2.java
+Description: A simple program that accepts a user input and greets the user by what they put in.
+Date: 08/31/2022
+Developer: Sam Biner
+*/
+
 import java.util.Scanner;
 class HelloSayerV2 {
   public static void main(String args[]){
@@ -5,6 +12,5 @@ class HelloSayerV2 {
     Scanner myInput = new Scanner( System.in );
     String inputName = myInput.nextLine();
     System.out.println("\nHello there, " + inputName);
-    System.out.println("\nHello, World! \n");
   }
 }
